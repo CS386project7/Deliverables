5a715c552a04490669c23ecb24504c9afeeca23f
yb001019 <68016456+yb001019@users.noreply.github.com>
2020-08-03 23:40:54 -0700

    Update CommentTest.java

--- CommentTest.java
+++ CommentTest.java
@@ -1,1 +1,16 @@
-abc
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CommentTest
+{
+    @Test
+    public void testAddComment()
+    {
+        String c = "Great video";
+        Comment comment = new Comment();
+        assertEquals("Great video", comment.addComment(c));
+    }
+
+}
